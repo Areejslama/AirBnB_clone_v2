a49b01426ac9d907fb6e368f8acde2237df4bc94
Areejslama <Areegahmed449@gmail.com>
2024-05-03 18:11:27 -0700

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,11 +7,11 @@ from fabric.api import *
 def do_pack():
     """define function"""
     t = datetime.now()
-    arc = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + tgz
-    local("mkdir -p versions")
-    my_arc = "tar -czvf versions /{}".format(arc)
+    arc = 'web_static_' + t.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    my_arc = local('tar -czvf versions/{} web_static'.format(arc))
     if my_arc is not None:
-        return my_arc
+        return arc
     else:
         return None
 
