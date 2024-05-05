d246db59cb5fd23d53c9abb70f289727593a6431
Areejslama <Areegahmed449@gmail.com>
2024-05-05 14:33:52 -0700

    do

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,16 +11,16 @@ def do_deploy(archive_path):
         return False
     try:
         new_arc = archive_path.split("/")[-1]
-        new_name = new_arc.split(".")[0]
-        path = '/data/web_static/releases/'
+        new_name = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, new_name))
-        run('tar -xzf /tmp/{} -C {}{}'.format(new_arc, path, new_name))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(new_arc, path, new_name))
         run('rm /tmp/{}'.format(new_arc))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, new_name))
-        run('rm -rf {}{}/web_static'.format(path, new_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, new_name))
+        run('rm -rf {}{}/web_static'.format(path, new_arc))
         run('rm -rf /data/web_static/current')
-        run('ln -sf {}{}/ /data/web_static/current'.format(path, new_name))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, new_name))
         return True
-    except Exception:
+    except:
         return False
