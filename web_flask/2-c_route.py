f4a1f9d6415ad30560ad155056b781cf3616c25b
Areejslama <Areegahmed449@gmail.com>
2024-05-18 06:35:01 -0700

    2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this script to start flask"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def welcome():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_text(text):
+    new_text = text.replace("_", " ")
+    return f'c {text}'
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
